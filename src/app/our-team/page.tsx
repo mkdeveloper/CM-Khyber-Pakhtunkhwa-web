b771d45ae3eef31c8cc595de79082e11b3765fe7
mkdeveloper <khubaib.mk@gmail.com>
2024-08-25 12:09:42 +0500

    remove constant to other file, added wania to team

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -1,73 +1,12 @@
-import Image from "next/image";
-import ZiaKhan from "../../../public/kpk/team/ziakhan.webp";
-import DaniyalNagori from "../../../public/kpk/team/daniyal_1.jpeg";
-import SaadIrfan from "../../../public/kpk/team/saad_irfan.jpg";
-import KhubaibKhan from "../../../public/kpk/team/khubaib.png";
-import SajidKhan from "../../../public/kpk/team/sajid_khan.jpeg";
-import Lab from "../../../public/kpk/team/lab.webp";
-const stats = [
-  { label: "Students learning in PIAIC", value: "500,000" },
-  { label: "Students learning in GIAIC", value: "500,000" },
-];
-const values = [
-  {
-    name: "Innovation",
-    description:
-      "We value innovation and encourage our students to think outside the box, explore new ideas, and push the boundaries of what is possible in the field of Generative AI. Through a culture of innovation, we aim to foster creativity, curiosity, and a passion for learning, empowering individuals to make meaningful contributions to the industry.",
-  },
-  {
-    name: "Collaboration",
-    description:
-      "We believe that collaboration is essential to success in the field of Generative AI. Our program emphasizes teamwork, communication, and the exchange of ideas, creating a supportive and collaborative learning environment. By working together, we can leverage our collective strengths and expertise to achieve our shared goals.",
-  },
-  {
-    name: "Excellence",
-    description:
-      "We are committed to excellence in all aspects of our program, from our curriculum to our instructors to our facilities. We strive to provide the highest quality education and training, ensuring that our students are well-prepared to excel in the field of Generative AI",
-  },
-  {
-    name: " Diversity and Inclusion",
-    description:
-      "We value diversity and inclusion and believe that a diverse community of learners is essential to the success of our program. We are committed to creating a welcoming and inclusive environment that celebrates differences and fosters a sense of belonging for all students.",
-  },
-  {
-    name: "Ethics and Responsibility",
-    description:
-      "We recognize the importance of ethical considerations in the development and application of Generative AI technologies. Our program emphasizes the importance of responsible and ethical practices, ensuring that our students are well-equipped to navigate the complex ethical considerations that arise in the field of Generative AI.",
-  },
-  {
-    name: "Lifelong Learning",
-    description:
-      "We believe that learning is a lifelong journey and that our students should be equipped with the skills and knowledge necessary to continue learning and growing throughout their careers. Our program emphasizes the importance of ongoing learning and professional development, empowering individuals to stay up-to-date with the latest developments in the field of Generative AI.",
-  },
-];
-const team = [
-  {
-    name: "Zia Khan",
-    role: "CEO of Panacloud / COO PIAIC",
-    imageUrl: ZiaKhan,
-  },
-  {
-    name: "Daniyal Nagori",
-    role: "CEO of PIAIC / CEO GIAIC",
-    imageUrl: DaniyalNagori,
-  },
-  {
-    name: "Muhammad Khubaib",
-    role: "Trainer PIAIC, Freelancer",
-    imageUrl: KhubaibKhan,
-  },
-  {
-    name: "Sajid Khan",
-    role: "Trainer PIAIC, Freelancer",
-    imageUrl: SajidKhan,
-  },
-  {
-    name: "Saad Irfan",
-    role: "Trainer PIAIC, Freelancer",
-    imageUrl: SaadIrfan,
-  },
-];
+import Image from 'next/image';
+import ZiaKhan from '../../../public/kpk/team/ziakhan.webp';
+import DaniyalNagori from '../../../public/kpk/team/daniyal_1.jpeg';
+import SaadIrfan from '../../../public/kpk/team/saad_irfan.jpg';
+import KhubaibKhan from '../../../public/kpk/team/khubaib.png';
+import SajidKhan from '../../../public/kpk/team/sajid_khan.jpg';
+import WaniaKazmi from '../../../public/kpk/team/Wania.png';
+import Lab from '../../../public/kpk/team/lab.webp';
+import { OurTeamCards } from '@/constants/our-team';
 
 export default function OurTeam() {
   return (
@@ -112,7 +51,7 @@ export default function OurTeam() {
               className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#1eb2a6] to-[#1eb2a6] opacity-30"
               style={{
                 clipPath:
-                  "polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)",
+                  'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
               }}
             />
           </div>
@@ -135,6 +74,7 @@ export default function OurTeam() {
                   </p>
                 </div>
                 <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+                  {/* col-1 */}
                   <div className="hidden sm:block ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                     <div className="relative">
                       <Image
@@ -147,6 +87,7 @@ export default function OurTeam() {
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                     </div>
                   </div>
+                  {/* col-2 */}
                   <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
                     <div className="relative">
                       <Image
@@ -179,6 +120,7 @@ export default function OurTeam() {
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                     </div>
                   </div>
+                  {/* col-3 */}
                   <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                     <div className="relative">
                       <Image
@@ -200,6 +142,16 @@ export default function OurTeam() {
                       />
                       <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                     </div>
+                    <div className="relative">
+                      <Image
+                        width={500}
+                        height={500}
+                        src={WaniaKazmi}
+                        alt="Wania Kazmi"
+                        className="aspect-[2/3] w-full rounded-xl h-full bg-gray-900/5 object-cover shadow-lg"
+                      />
+                      <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -213,9 +165,9 @@ export default function OurTeam() {
             <h2 className="text-3xl font-bold tracking-tight text-custom-color sm:text-4xl">
               OUR MISSION
             </h2>
-            <div className="mt-6 flex flex-col gap-x-8 gap-y-20 lg:flex-row">
-              <div className="lg:w-full lg:max-w-2xl lg:flex-auto">
-                <p className="text-xl leading-8 text-gray-600">
+            <div className="mt-6 flex mx-auto gap-x-8 gap-y-20">
+              <div className="lg:w-full lg:max-w-5xl lg:flex-auto">
+                <p className="paragraph-center">
                   The CM KPK Initiative for Generative AI and Cloud Computing
                   program is dedicated to preparing individuals for the
                   transformative era of Generative AI.To empower individuals
@@ -231,23 +183,6 @@ export default function OurTeam() {
                   expertise to develop world-class AI applications and
                   solutions.
                 </p>
-              </div>
-              <div className="lg:flex lg:flex-auto lg:justify-center">
-                <dl className="w-64 space-y-8 xl:w-80">
-                  {stats.map((stat) => (
-                    <div
-                      key={stat.label}
-                      className="flex flex-col-reverse gap-y-4"
-                    >
-                      <dt className="text-base leading-7 text-gray-600">
-                        {stat.label}
-                      </dt>
-                      <dd className="text-5xl font-semibold tracking-tight text-custom-color">
-                        {stat.value}
-                      </dd>
-                    </div>
-                  ))}
-                </dl>
               </div>
             </div>
           </div>
@@ -313,7 +248,7 @@ export default function OurTeam() {
             </p>
           </div>
           <dl className="mx-auto  mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {values.map((value) => (
+            {OurTeamCards.map((value) => (
               <div
                 className="hover:bg-[#1eb2a6] duration-300  group rounded-xl p-3 border border-custom-color"
                 key={value.name}
@@ -332,3 +267,36 @@ export default function OurTeam() {
     </div>
   );
 }
+
+const team = [
+  {
+    name: 'Zia Khan',
+    role: 'CEO of Panacloud / COO PIAIC',
+    imageUrl: ZiaKhan,
+  },
+  {
+    name: 'Daniyal Nagori',
+    role: 'CEO of PIAIC / CEO GIAIC',
+    imageUrl: DaniyalNagori,
+  },
+  {
+    name: 'Muhammad Khubaib',
+    role: 'Trainer PIAIC, Freelancer',
+    imageUrl: KhubaibKhan,
+  },
+  {
+    name: 'Sajid Khan',
+    role: 'Trainer PIAIC, Freelancer',
+    imageUrl: SajidKhan,
+  },
+  {
+    name: 'Saad Irfan',
+    role: 'Trainer PIAIC, Freelancer',
+    imageUrl: SaadIrfan,
+  },
+  {
+    name: 'Wania Kazmi',
+    role: 'Trainer PIAIC, Freelancer',
+    imageUrl: WaniaKazmi,
+  },
+];
