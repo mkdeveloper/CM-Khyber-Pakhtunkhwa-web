a53a1b23d48d5c6e1bfbdb606f4861839d2ca0aa
mkdeveloper <khubaib.mk@gmail.com>
2024-08-25 10:23:29 +0500

    fixed imports

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,56 +1,57 @@
-'use client'
-import { Dialog, NavButton, NavigationList } from './navbar'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
-import Image from 'next/image'
-import KP_Logo from '../../../public/kpk/kp_logo.png'
+'use client';
+import { Dialog, NavButton } from './navbar';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+import KP_Logo from '../../../public/kpk/kp_logo.png';
+import { NavigationList } from '@/constants/NavigationLinks';
 const Header = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <div
       className={`mx-auto max-w-[1300px] flex h-full w-full justify-between px-3 py-5 lg:px-10 lg:py-6 max-md:max-w-full max-md:flex-wrap md:justify-center xl:justify-between ${
         pathname === '/' ? 'hidden' : ''
       }`}
     >
-      <div className='my-auto flex items-stretch justify-between gap-12 max-md:max-w-full max-md:flex-wrap'>
-        <div className='flex lg:flex-1 outline-none'>
-          <Link href='/' className='-m-1.5 p-1.5 outline-none'>
-            <span className='sr-only'>
+      <div className="my-auto flex items-stretch justify-between gap-12 max-md:max-w-full max-md:flex-wrap">
+        <div className="flex lg:flex-1 outline-none">
+          <Link href="/" className="-m-1.5 p-1.5 outline-none">
+            <span className="sr-only">
               KPK Initiative for Generative AI and Cloud Computing
             </span>
             <Image
               //   placeholder='blur'
               width={100}
               height={100}
-              className=' h-28 w-auto outline-none'
+              className=" h-28 w-auto outline-none"
               src={KP_Logo}
-              alt='KPK GOV LOGO'
+              alt="KPK GOV LOGO"
             />
           </Link>
         </div>
       </div>
-      <div className='my-auto hidden  grow items-center justify-center md:gap-x-5 lg:gap-x-7 xl:gap-x-12 self-start text-sm lg:text-base font-medium leading-5 text-zinc-800 max-md:max-w-full md:flex md:text-[13px]'>
+      <div className="my-auto hidden  grow items-center justify-center md:gap-x-5 lg:gap-x-7 xl:gap-x-12 self-start text-sm lg:text-base font-medium leading-5 text-zinc-800 max-md:max-w-full md:flex md:text-[13px]">
         {NavigationList.map((item) => (
           <NavButton
-            className='after:hover:bg-custom-color leading-6 font-medium'
+            className="after:hover:bg-custom-color leading-6 font-medium"
             key={item.name}
             name={item.name}
             href={item.href}
           />
         ))}
       </div>
-      <div className='flex items-center md:hidden cursor-pointer '>
+      <div className="flex items-center md:hidden cursor-pointer ">
         <Dialog />
       </div>
-      <div className='hidden items-center max-md:max-w-full max-md:flex-wrap md:flex'>
+      <div className="hidden items-center max-md:max-w-full max-md:flex-wrap md:flex">
         <NavButton
-          name='APPLY NOW'
-          className='after:hover:w-0 bg-custom-color  py-3 px-4 rounded-md text-white hover:text-custom-color hover:bg-transparent border border-custom-color duration-300 md:text-xs lg:text-base'
-          href='/registration'
+          name="APPLY NOW"
+          className="after:hover:w-0 bg-custom-color  py-3 px-4 rounded-md text-white hover:text-custom-color hover:bg-transparent border border-custom-color duration-300 md:text-xs lg:text-base"
+          href="/registration"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
