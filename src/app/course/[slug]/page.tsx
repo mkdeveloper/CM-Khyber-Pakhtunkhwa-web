d810db6f44933352ec6c8e1d90a8416e3704c26a
mkdeveloper <khubaib.mk@gmail.com>
2024-08-26 11:15:58 +0500

    fixed generateStaticParams Issue

--- src/app/course/[slug]/page.tsx
+++ src/app/course/[slug]/page.tsx
@@ -1,41 +1,60 @@
-import { possibleRoutes } from '@/constants/constants'
-import { CourseData } from '@/types/types'
-import { ForwardRequest, GetChildComponent } from '@/lib/helper-functions'
+import { possibleRoutes } from '@/constants/constants';
+import { CourseData } from '@/types/types';
+import { GetChildComponent } from '@/lib/helper-functions';
+
+function ForwardRequest(pathname: string) {
+  switch (pathname) {
+    case 'introduction':
+      return 'course/introduction';
+    case 'q1':
+    case 'q2':
+    case 'q3':
+      return `course/core/${pathname}`;
+    case 'q4':
+    case 'q5':
+    case 'q6':
+      return `course/mastery/${pathname}`;
+    case 'q7':
+      return `course/physical/${pathname}`;
+    default:
+      return `course/optional/${pathname}`;
+  }
+}
 
 const getData = async ({ pathname }: { pathname: string }) => {
   // const baseUrl =
   //   process.env.NODE_ENV === 'development'
   //     ? process.env.DEV_BASE_URL
   //     : process.env.PROD_BASE_URL
-  const baseUrl = process.env.BASE_URL
+  const baseUrl = process.env.BASE_URL;
   if (!baseUrl) {
-    throw new Error('BASE_URL environment variable is not set')
+    throw new Error('BASE_URL environment variable is not set');
   }
 
-  // const pathName = ForwardRequest(pathname)
-  // console.log(pathName);
+  const pathName = ForwardRequest(pathname);
+  console.log(pathName);
 
   const res = await fetch(`${baseUrl}/api/${pathname}`, {
     cache: 'force-cache',
-  })
+  });
 
   if (!res.ok) {
-    console.log(res)
-    throw new Error('Failed to fetch data')
+    console.log(res);
+    throw new Error('Failed to fetch data');
   }
 
-  const data: CourseData = await res.json()
+  const data: CourseData = await res.json();
 
-  return data
-}
+  return data;
+};
 
 const CourseContent = async ({ params }: { params: { slug: string } }) => {
   if (possibleRoutes.includes(params.slug)) {
-    const courseData = await getData({ pathname: params.slug })
+    const courseData = await getData({ pathname: params.slug });
 
     return (
-      <div className='mx-2 px-2 md:mx-5 md:px-10 pb-20 pt-10 bg-[#1eb2a6]/10 rounded-3xl min-h-dvh'>
-        <h1 className=' text-custom-color'>{courseData.mainTitle}</h1>
+      <div className="mx-2 px-2 md:mx-5 md:px-10 pb-20 pt-10 bg-[#1eb2a6]/10 rounded-3xl min-h-dvh">
+        <h1 className=" text-custom-color">{courseData.mainTitle}</h1>
         <p
           className={`${
             courseData.quarter.includes('Master the Future')
@@ -46,35 +65,35 @@ const CourseContent = async ({ params }: { params: { slug: string } }) => {
           {courseData.quarter}
         </p>
         {courseData.description.map((description, index) => (
-          <p key={index} className='leading-7 [&:not(:first-child)]:mt-6'>
+          <p key={index} className="leading-7 [&:not(:first-child)]:mt-6">
             {description}
           </p>
         ))}
         {courseData.outline.map((outline, index) => (
           <div key={index}>
-            <h3 className='mt-5'>{outline.title}</h3>
+            <h3 className="mt-5">{outline.title}</h3>
             {outline.children.map((child, childIndex) => (
-              <div className='py-1' key={childIndex}>
+              <div className="py-1" key={childIndex}>
                 {GetChildComponent(child)}
               </div>
             ))}
           </div>
         ))}
       </div>
-    )
+    );
   } else {
     return (
-      <div className='flex h-[50vh] w-full items-center justify-center text-2xl font-semibold text-zinc-800'>
+      <div className="flex h-[50vh] w-full items-center justify-center text-2xl font-semibold text-zinc-800">
         404 Not Found
       </div>
-    )
+    );
   }
-}
+};
 
-export default CourseContent
+export default CourseContent;
 
 export async function generateStaticParams() {
   return possibleRoutes.map((route) => ({
-    slug: route,
-  }))
+    slug: route.split('/').pop(),
+  }));
 }
