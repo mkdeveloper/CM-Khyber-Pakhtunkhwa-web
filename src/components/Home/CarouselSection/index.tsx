807673e18af71260ca06592a2a34d18493527b2d
mkdeveloper <khubaib.mk@gmail.com>
2024-08-31 14:03:04 +0500

    changed AI to complete form, fixed whatsapp button for smaller screens

--- src/components/Home/CarouselSection/index.tsx
+++ src/components/Home/CarouselSection/index.tsx
@@ -1,25 +1,26 @@
-import Link from 'next/link'
-import { CarouselPlugin } from './Carousel'
-import ParagraphImage from '../../../../public/kpk/Generative-AI-ChatGPT.webp'
-import Image from 'next/image'
+import Link from 'next/link';
+import { CarouselPlugin } from './Carousel';
+import ParagraphImage from '../../../../public/kpk/Generative-AI-ChatGPT.webp';
+import Image from 'next/image';
 export default function CarouselSection() {
   return (
-    <div className='bg-white py-12'>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='mx-auto lg:max-w-6xl max-w-3xl  md:text-center'>
-          <h2 className='text-3xl font-bold tracking-tight text-custom-color sm:text-4xl text-center'>
-            Become a Certified Cloud Native Applied Generative AI Engineer
+    <div className="bg-white py-12">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto lg:max-w-6xl max-w-3xl  md:text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-custom-color sm:text-4xl text-center">
+            Become a Certified Cloud Native Applied Generative Artificial
+            Intelligence Engineer
           </h2>
           {/* <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Mastering Cloud Native and Physical AI for the Next Technological
             Frontier
           </p> */}
-          <p className='mt-6 paragraph-center'>
-            The future of Generative AI and cloud-native computing is poised to
-            ignite the most transformative industrial revolution in human
-            history, projected to drive a{' '}
+          <p className="mt-6 paragraph-center">
+            The future of Generative Artificial Intelligence and cloud-native
+            computing is poised to ignite the most transformative industrial
+            revolution in human history, projected to drive a{' '}
             <Link href={'https://www.youtube.com/watch?v=e5Zol4RYq2o'}>
-              $100 trillion economy
+              $1.8 trillion economy
             </Link>
             . As these technologies converge, they will revolutionise every
             industry, from healthcare and finance to manufacturing and
@@ -31,28 +32,28 @@ export default function CarouselSection() {
             scalable systems will be at the heart of global progress, driving
             the biggest leap forward in the history of mankind.
           </p>
-          <div className=' mt-6 max-w-4xl mx-auto '>
+          <div className=" mt-6 max-w-4xl mx-auto ">
             <Image
               width={1920}
               height={1080}
               src={ParagraphImage}
-              alt=''
-              className='aspect-[3/2] md:aspect-[4/2] object-cover rounded-3xl'
+              alt=""
+              className="aspect-[3/2] md:aspect-[4/2] object-cover rounded-3xl"
             />
           </div>
-          <p className='mt-6 paragraph-center'>
+          <p className="mt-6 paragraph-center">
             Today&apos;s pivotal technological trends are Cloud Native (CN),
-            Generative AI (GenAI), and Physical AI. Cloud Native technology
-            offers a scalable and dependable platform for application operation,
-            while AI equips these applications with intelligent, human-like
-            capabilities. Physical AI aims to bridge the gap between digital
-            intelligence and physical capability, creating systems that can
-            understand and interact with the world in a human-like manner. Our
-            aim is to train you to excel as a Cloud Native Applied Generative
-            and Physical AI developer globally.
+            Generative Artificial Intelligence (GenAI), and Physical AI. Cloud
+            Native technology offers a scalable and dependable platform for
+            application operation, while AI equips these applications with
+            intelligent, human-like capabilities. Physical AI aims to bridge the
+            gap between digital intelligence and physical capability, creating
+            systems that can understand and interact with the world in a
+            human-like manner. Our aim is to train you to excel as a Cloud
+            Native Applied Generative and Physical AI developer globally.
           </p>
 
-          <p className='mt-2 paragraph-center'>
+          <p className="mt-2 paragraph-center">
             {/* As the world rapidly embraces Cloud Native and Physical AI
             technologies, we stand on the edge of a new era of innovation.These
             advancements are set to revolutionize industries by enabling
@@ -67,13 +68,13 @@ export default function CarouselSection() {
             to develop world-class AI applications, driving both technological
             innovation and economic growth in the region.
           </p>
-          <p className='mt-2 paragraph-center'>
+          <p className="mt-2 paragraph-center">
             Join us and become part of the next generation of tech leaders.
-            <span className='font-semibold'>The future is yours to shape!</span>
+            <span className="font-semibold">The future is yours to shape!</span>
           </p>
         </div>
         <CarouselPlugin />
       </div>
     </div>
-  )
+  );
 }
--- src/components/Home/CourseSection/index.tsx
+++ src/components/Home/CourseSection/index.tsx
@@ -1,20 +1,20 @@
-import CourseLevel from './CourseLevel'
-import { CoreLevel, MasteryLevel, PhysicalAI } from './CourseData'
+import CourseLevel from './CourseLevel';
+import { CoreLevel, MasteryLevel, PhysicalAI } from './CourseData';
 
 export default function CourseSection() {
   return (
-    <div className='bg-white py-24'>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='mx-auto max-w-6xl text-center'>
+    <div className="bg-white py-24">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-6xl text-center">
           <h2
             style={{ lineHeight: 'inherit' }}
-            className='mt-2 text-3xl font-bold tracking-tight text-custom-color sm:text-4xl'
+            className="mt-2 text-3xl font-bold tracking-tight text-custom-color sm:text-4xl"
           >
             Master the Future
-            <br /> Getting You Ready For The $100 Trillion AI Industrial
-            Revolution
+            <br /> Getting You Ready For The $1.8 Trillion Artificial
+            Intelligence Industrial Revolution
           </h2>
-          <p className='mt-2 paragraph-center text-justify'>
+          <p className="mt-2 paragraph-center text-justify">
             Embark on a 1-year learning journey, with 9 months of advanced
             training, structured across 7 quarters to acquire advanced
             technology skills. The first 3 core quarters provide a solid
@@ -24,7 +24,7 @@ export default function CourseSection() {
             technologies ensures you stay at the forefront of the AI revolution.
             Regular breaks throughout the program allow for rest and
             reinforcement of concepts, positioning you to unlock opportunities
-            in the lucrative $100 trillion industry.
+            in the lucrative $1.8 trillion industry.
             <span>
               The detailed outline of each course level and its quarters is
               provided below.
@@ -36,5 +36,5 @@ export default function CourseSection() {
         <CourseLevel level={PhysicalAI} />
       </div>
     </div>
-  )
+  );
 }
