eea8c2792389de719e265f2a7d6c98c5d3addf9b
mkdeveloper <khubaib.mk@gmail.com>
2024-09-01 12:24:46 +0500

    used neon driver instead vercel for db mgmt

--- src/lib/drizzle.ts
+++ src/lib/drizzle.ts
@@ -1,4 +1,9 @@
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 
-import { sql } from "@vercel/postgres";
+const sql = neon(process.env.POSTGRES_URL!);
 export const db = drizzle(sql);
+// import { drizzle } from "drizzle-orm/vercel-postgres";
+
+// import { sql } from "@vercel/postgres";
+// export const db = drizzle(sql);
